1a56ac937d370206d549f86c0701530f3a9a13a7
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2025-07-27 16:13:14 +0300

    fix request.py

--- homework_3/python/request.py
+++ homework_3/python/request.py
@@ -1,7 +1,7 @@
+import json
 import sys
 
 import requests
-
 
 
 url = "http://localhost:8082/command"
@@ -10,21 +10,32 @@ params = {
   "initiator": "WAYLAND_YUTANI_OFFICER",
   "priority": "COMMON",
   "author": "System",
-  "time": "2025-07-27T14:30:00",
+  "time": "2025-07-27T14:30:00Z",
 }
 
 try:
   response = requests.post(url, params=params)
   response.raise_for_status()
 
-  print(
-    "Request successful!\nResponse:\n\t"
-    f"Status:{response.status_code}\n\t"
-    f"Text:{response.text}\n\t"
-    f"URL:{response.url}\n\t"
-    f"JSON:{response.json()}\n\t"
-  )
+  print("Request successful! Response:")
+
+  print(f"\tStatus: {response.status_code}")
+  print(f"\tURL: {response.url}")
+
+  try:
+    json_data = response.json()
+    print("\tJSON:")
+    print(json.dumps(json_data, indent=2))
+
+  except ValueError:
+    print(f"\tNon-JSON Response: {response.text}")
 
 except requests.exceptions.RequestException as e:
-  print(f"Request failed:\n\t{e}")
+  print("\nRequest failed:")
+  print(f"Error: {e}")
+
+  if hasattr(e, "response") and e.response is not None:
+    print(f"Status Code: {e.response.status_code}")
+    print(f"Response Text: {e.response.text}")
+
   sys.exit(1)
