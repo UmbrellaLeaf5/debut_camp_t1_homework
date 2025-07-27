c2b27690751777c8cf9bdb94b2c528f0847583a7
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2025-07-27 23:12:33 +0300

    fix todo with kafka validation

--- homework_3/synthetic-human-core-starter/src/main/java/io/github/UmbrellaLeaf5/synth/core/audit/component/KafkaAuditComponent.java
+++ homework_3/synthetic-human-core-starter/src/main/java/io/github/UmbrellaLeaf5/synth/core/audit/component/KafkaAuditComponent.java
@@ -1,7 +1,7 @@
 package io.github.UmbrellaLeaf5.synth.core.audit.component;
 
 import io.github.UmbrellaLeaf5.synth.core.audit.model.AuditEvent;
-import io.github.UmbrellaLeaf5.synth.core.audit.properties.AuditConfigurationProperties;
+import io.github.UmbrellaLeaf5.synth.core.audit.properties.kafka.KafkaAuditConfigurationProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class KafkaAuditComponent implements AuditComponent {
   private final KafkaTemplate<String, AuditEvent> kafkaTemplate;
-  private final AuditConfigurationProperties auditConfigurationProperties;
+  private final KafkaAuditConfigurationProperties auditConfigurationProperties;
 
   @Override
   public void audit(AuditEvent event) {
---
+++ homework_3/synthetic-human-core-starter/src/main/java/io/github/UmbrellaLeaf5/synth/core/audit/properties/AuditMode.java
@@ -0,0 +1,3 @@
+package io.github.UmbrellaLeaf5.synth.core.audit.properties;
+
+public enum AuditMode { KAFKA, CONSOLE }
